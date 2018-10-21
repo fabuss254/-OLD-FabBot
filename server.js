19955259bb267b7a4ab7af513b887722d7c0928c
fabuss254 <fabuss254@outlook.fr>
2018-10-21 22:16:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,19 @@
 /*
-    Nom : Slenderbot
-    Description : Le bot du serveur de la Slendarmy !
-    Auteurs : ZENFIX#8575 | Fabuss254#9232
+    Nom : Fabbot
+    Description : Bot modulaire, permet d'utiliser des modules supplémentaire pour protéger et elargir sont serveur.
+    Auteur : Fabuss254#9232
 */
 
 //VARIABLE CONST
 
 const Discord = require("discord.js");
 const prefix = "f.";
-const fs = require("fs");
 const bot = new Discord.Client();
 const debug = true;
 
 //TRAITEMENT
 
-fs.readdir("./CoreSc/", (err, files) => {
 
-  if(err) console.log(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
-    return;
-  }
-
-  jsfile.forEach((f, i) =>{
-    let props = require(`./CoreSc/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
-  });
-});
 
 //EVENEMENT
 
@@ -36,7 +21,7 @@ bot.on('ready', () => {
     bot.user.setStatus('Online'); // En ligne : 'Online' | Inactif : 'idle' | Ne pas déranger : 'dnd' | Invisible 'invisible'
     bot.user.setActivity(prefix + "help | " + bot.guilds.size + " serveurs | " + bot.users.size + " utilisateurs", {
         'type': 'STREAMING',
-        'url': "https://twitch.tv/ZENFIX_"
+        'url': "https://twitch.tv/fabuss255"
     },
     console.log("\nBot connecté !"));
     console.log("\nInfos :\nNombre de serveurs : " + bot.guilds.size + "\nNombre d'utilisateurs : " + bot.users.size);
@@ -49,31 +34,18 @@ bot.on('ready', () => {
 
 
 bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.content.startsWith(prefix)){
-      if (debug){
-          if (message.author.id != "178131193768706048" && message.author.id != "274240989944610827" && message.author.id != "337863324983230474"){
-              message.delete()
-              var errorcommand_embed = new Discord.RichEmbed ()
-                   .setColor("#FF0000")
-                   .addField("Désolé !", "Il y a une erreur avec votre requête !")
-                   .addField("Raison :", "• Le bot est actuellement en mode correction de bugs, ce qui signifie que seulement certaines personnes peuvent utiliser le bot actuellement.")
-              message.channel.send(errorcommand_embed)
-              return
-          }
-      }
-      let args = message.content.split(" ");
-      let commandfile = bot.commands.get(args[0].substring(prefix.length).toLowerCase());
-      if(commandfile){ 
-          commandfile.run(bot,message,args,prefix)
-      }else{
-          message.delete()
-          var errorcommand_embed = new Discord.RichEmbed ()
-               .setColor("#FF0000")
-               .addField("Désolé !", "Il y a une erreur avec votre requête !")
-               .addField("Raison :", "• La commande ``" + args[0] + "`` n'éxiste pas ! Vous pouvez faire ``" + prefix + "help`` pour obtenir la liste des commandes disponible !")
-         message.channel.send(errorcommand_embed)
-      };
+  if (message.author.equals(bot.user)) return;
+  var args = message.content.substring(prefix.length).split (" ");
+  if (!message.content.startsWith(prefix)) return;
+  switch (args[0].toLowerCase()) {
+      case "say":
+          message.channel.send(message.content.substring(6));
+          message.delete(100);
+          break;
+          
+      case default:
+          message.channel.send("Commande inconnu");
+          break;
   };
 });
 
