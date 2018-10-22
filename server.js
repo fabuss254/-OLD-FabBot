9fc04a27a8799242e0296a70a84ae7a74e3ba35b
fabuss254 <fabuss254@outlook.fr>
2018-10-22 15:15:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -30,42 +30,10 @@ var HelpEmbed = new Discord.RichEmbed()
 .setTitle("__**Liste des commandes**__")
 .setDescription("**Voici la listes des commandes disponibles au bot, trier par modules**")
 .addField("Normal","**Cmd** - Donne la liste des commandes\n**Help <commande>** - Donne des infos suplémentaire sur une commande\n**Install <module>** - Installer un module (Voir si dessous pour la liste des modules disponibles)\n**Uninstall <module>** - Desinstaller un module")
-.addField("VCS","**Send** - Envoyer un message simple dans le vcs\n**Channel** - Regler le salon qui est utiliser pour vcs")
+.addField("VCS","**Send <msg>** - Envoyer un message simple dans le vcs\n**Channel** - Regler le salon qui est utiliser pour vcs\n")
 .addField("Capcha","**Verify** - Demander une verification (manuelle)\n**Auto-Verif** - [ADMIN]Mettre le bot en verification automatique\n**SetRole** - [ADMIN]Regler le role aprés verification\n**WelcomeMessage** - [ADMIN]Regler le message de bienvenue (envoyer quand la verification automatique est activer)")
 .addField("Musique","Work in progress...");
-    
-/*    
-{
-  "embed": {
-    "title": "__**Liste des commandes**__",
-    "description": "**Voici la listes des commandes disponibles au bot, trier par modules**",
-    "color": 0,
-    "author": {
-      "name": "author name",
-      "url": "https://discordapp.com",
-      "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
-    },
-    "fields": [
-      {
-        "name": "Normal",
-        "value": "**Help <commande>** - Donne des infos suplémentaire sur une commande\n**Install <module>** - Installer un module (Voir si dessous pour la liste des modules disponibles)\n**Uninstall <module>** - Desinstaller un module"
-      },
-      {
-        "name": "VCS",
-        "value": "**Send** - Envoyer un message simple dans le vcs\n**Channel** - Regler le salon qui est utiliser pour vcs"
-      },
-      {
-        "name": "Capcha",
-        "value": "**Verify** - Demander une verification (manuelle)\n**Auto-Verif** - [ADMIN]Mettre le bot en verification automatique\n**SetRole** - [ADMIN]Regler le role aprés verification\n**WelcomeMessage** - [ADMIN]Regler le message de bienvenue (envoyer quand la verification automatique est activer)"
-      },
-      {
-        "name": "Musique",
-        "value": "Work in progress..."
-      }
-    ]
-  }
-}
-*/
+
 //EVENEMENT
 
 bot.on('ready', function(){
@@ -163,6 +131,36 @@ bot.on("message", function(message){
                message.channel.send("Success!").then(function(message){message.delete(5000)});
               };
           };
+          break;
+          
+      case "install":
+          if (message.member.hasPermission("ADMINISTRATOR", false, true, true)){
+              if (!args[1]){
+                  message.channel.send("Merci de donner le module à installer, **Liste des modules disponible**:\nVCS")
+              }else{
+                  if(args[1].toLowerCase() == "vcs"){
+                      var docRef = db.collection("Servers").doc(message.guild.id);
+                      docRef.get().then(function(doc){
+                          if (doc.data().Modules.VCS == false){
+                              message.channel.send("Activation du module...").then(function(message){
+                                docRef.set({
+                                    Modules: {VCS: true},
+                                    VCS: {Channel: message.channel.id}
+                                  }, {merge: true}).then(function(){
+                                    message.edit("Module installer! Ce channel va être utiliser comme salon vcs, si vous souhaitez changer, faite 'f.VCSChannel' dans le channel souhaitée."
+                                  }));
+                          }else{
+                              
+                          }
+                      });
+                      
+                  }else{
+                      message.channel.send("Module non reconnu!")
+                  }
+              }
+          }else{
+              message.channel.send("Tu n'as pas la permission d'utiliser cette commande!").then(function(message){message.delete(5000)});
+          }
           break;
           
       default:
